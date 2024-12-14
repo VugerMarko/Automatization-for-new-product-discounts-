a04dd7f76594ee1234a8f5d79a3ceffa98c3b742
Marko Vuger <markovugerlol@gmail.com>
2024-12-14 14:25:50 +0100

    Added: smtp protocol that sends a flagged product to an email

--- dataManipulationScript.py
+++ dataManipulationScript.py
@@ -1,5 +1,39 @@
 import csv
 from main import *
+import smtplib
+from email.mime.text import MIMEText
 
 
+smtp_server = "smtp.gmail.com"
+port = 465
 
+sender = "markovugerlol@gmail.com"
+password = "cpbl hpis jblc wkki"
+
+recipient = "markovugerlol@gmail.com"
+
+subject = {}
+
+subject = "".join(f"{attr_name}: {attr_value}\n" for attr_name, attr_value in flagProductObj.__dict__.items())
+
+try:
+    server = smtplib.SMTP_SSL(smtp_server, port)
+    server.login(sender, password)
+    
+    message = MIMEText(subject)
+    message["Subject"] = "SSL Test Email"
+    message["From"] = sender
+    message["To"] = recipient
+    server.sendmail(sender, recipient, message.as_string())
+    
+    print("Email sent succesfully!")
+
+except smtplib.SMTPAuthenticationError as e:
+    print(f"Authentication error: {e}")
+    
+except Exception as e:
+    print(f"Error: {e}")
+
+finally:
+    server.quit()
+    
--- main.py
+++ main.py
@@ -2,18 +2,34 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-
 base_url = "https://www.muziker.hr/elektricne-gitare-svi-oblici"
 
 page = 1
 n = 0
+
+class flaggedProduct:
+    flagged_instance = []
+    
+    def __init__(self, iD = -1, name = "", model = "", price = "", status = "", old_price = "", discount = ""):
+        self.iD = iD
+        self.name = name
+        self.model = model
+        self.price = price
+        self.old_price = old_price
+        self.discount = discount
+        self.status = status
+
+
+flagged_model = ""
+
+flagProductObj = flaggedProduct()
 
 with open('guitar.csv', 'w', newline='', encoding='utf-8') as file:
         csvwriter = csv.writer(file)
     
         csvwriter.writerow(['ID', 'Guitar Name', 'Model', 'Price', 'Old Price', 'Discount', 'Status'])
     
-        while True:
+        while page < 2:
             url = ""
 
             if page != 1:
@@ -44,7 +60,7 @@ with open('guitar.csv', 'w', newline='', encoding='utf-8') as file:
                     print(name, model)
                     
                     price_tag = guitar.find('div', class_='price mzkr-mr-1')
-                    dual_price_tag = price_tag.find('div', class_='dual-price lh-1 mzkr-mb-1')
+                    dual_price_tag = price_tag.find('div', class_='dual-price lh-1 mzkr-mb-1') if price_tag else 'N/A'
                     
                     stock_status = guitar.find('div', class_='stock-status')
                     status = stock_status.text.strip() if stock_status else 'N/A'
@@ -64,14 +80,23 @@ with open('guitar.csv', 'w', newline='', encoding='utf-8') as file:
                             content_before_dual.append(content)
                         price = ''.join(str(c).strip() for c in content_before_dual if str(c).strip())
                             
+                    if n == 26:
+                        flagProductObj.iD = n
+                        flagProductObj.model = model
+                        flagProductObj.name = name
+                        flagProductObj.status = status
+                        flagProductObj.discount = discount
+                        flagProductObj.old_price = old
+                        flagProductObj.price = price
+            
                     csvwriter.writerow([n, name, model, price, old, discount, status])
 
                 if not guitars:
-                    print("no more products. Stopping pagination.")
+                    print("No more products. Stopping pagination.")
                     break
 
             else:
                 print(f"Failed to retrieve the page. Status code: {response.status_code}")
 
             page += 1
-
+            
